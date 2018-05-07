a39fe79230d99a885c5d335dcbf6ecf287eaf117
Erick Alvarenga <Erick.Alvarenga@nintendo.de>
2018-05-07 10:01:09 +0200

    make it the simplest extension possible

--- js/events.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', () => {
-  adjustPage()
-}, false)
-
-chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
-   if (msg.action == 'AdjustPage') {
-      adjustPage()
-   }
-});
