82e038c61200d7afb6f438576b4a2a939096a3db
Erick Patrick <github@erickpatrick.net>
2018-05-07 10:04:11 +0200

    make it simple again
    * WIP
    
    * makes all boxes on topic page same size and width for better reordering
    
    * updates page layout when navigation to other pages
    
    * fixes layout adjustments when navigating to other pages
    
    * fixes #1
    
    * cleans up background.js
    
    * make it the simplest extension possible

--- js/home.js
+++ js/home.js
@@ -1,45 +1,24 @@
-window.addEventListener('mext--home-event', () => {
-  if (isHomePage()) {
-    addClassToElement(document.querySelector('body'), 'mext--home')
+const topicStream = document.querySelector('.js-topicStream')
 
-    const extremeContainer = document.querySelector('.extreme-container12col')
-    const extremeAdaptiveSection = document.querySelectorAll('.extremeAdaptiveSection')
+const createDomMutationObserver = (node, callback, extraConfig = {}) => {
+  (new MutationObserver(callback)).observe(node, { childList: true, ...extraConfig })
+}
 
-    // make single column content
-    extremeContainer.classList.remove('u-flex')
-
-    // removes fixing of sidebar items
-    extremeAdaptiveSection.forEach((item, index) => {
-      item.classList.remove('js-sidebar')
-      delete item.dataset.scroll
+createDomMutationObserver(
+  topicStream,
+  () => {
+    const featuredForMemberItems = document.querySelectorAll('.svgIcon--star')
+    featuredForMemberItems.forEach(item => {
+      item
+        .parentElement
+        .parentElement
+        .parentElement
+        .parentElement
+        .parentElement
+        .parentElement
+        .parentElement
+        .classList
+        .add('make-it-shine')
     })
-
-    // moves sidebar items to top
-    const sidebarFragment = document.createDocumentFragment()
-    sidebarFragment.appendChild(document.querySelector('.extreme-container4col'))
-    extremeContainer.insertBefore(sidebarFragment, extremeContainer.firstChild)
-
-    createDomMutationObserver(
-      document.querySelector('.extreme-container8col'),
-      () => {
-        // removes featuredForMemberItems
-        const featuredForMemberItems = document.querySelectorAll('.svgIcon--star')
-        featuredForMemberItems.forEach(item => {
-          const parentNode = item.closest('.streamItem').parentNode
-
-          if (parentNode) {
-            parentNode.removeChild(item.closest('.streamItem'))
-          }
-        })
-
-        // removes heading titles that the next element is hidden
-        const sectionTitles = document.querySelectorAll('.streamItem--heading')
-        sectionTitles.forEach(section => {
-          if (!section.nextElementSibling || section.nextElementSibling.classList.contains('streamItem--heading')) {
-            section.parentNode.removeChild(section)
-          }
-        })
-      }
-    )
   }
-})
+)
